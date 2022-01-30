86b63c1fd8d126f8a61f8ac38f53e3b0e27d5e1e
kleveck <kcleveque@gmail.com>
2022-01-29 22:19:22 -0600

    added glass cards and a map to contact page

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,15 @@
-const cards = document.querySelectorAll(".cards__single");
-
-function flipCard() {
-  this.classList.toggle("flip");
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 29.163462624834157, lng: -95.42474040316289 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 19,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
 }
-cards.forEach((card) => card.addEventListener("click", flipCard));
- 
+
